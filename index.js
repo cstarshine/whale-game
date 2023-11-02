6e85b5108c357a6742d36ea9b299c797a1b3b13d
jychoi <jychoi@forbiz.co.kr>
2023-11-02 16:46:30 +0900

    add comment, code refactoring, add more kelp

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 var canvas;
 var ctx;
 
+var score = 0;
+var upBtnClicked = false;
+
 var whale = {
     x: 50,
     y: 20,
     status: 1,
     imgSrc: "asset/img/whale1.png",
     img: new Image(),
-    isCollision: function(obj){
-        if(this.x < obj.x+50 && this.x+40 > obj.x && this.y < obj.y+100 && this.y+40 > obj.y){
+    isCollision: function(obj) {
+        if(this.x < obj.x + 50 && this.x + 40 > obj.x && this.y < obj.y + 100 && this.y + 40 > obj.y){
             return true;
         }
         return false;
     }
 };
 
-var kelp = [];
+var kelp = [
+    {
+        name: "downKelp1",
+        x: 300,
+        y: 200
+        //img: new Image()
+    },
+    {
+        name: "upKelp1",
+        x: 300,
+        y: 0
+        //img: new Image()
+    },
+    {
+        name: "downKelp2",
+        x: 540,
+        y: 200
+        //img: new Image()
+    },
+    {
+        name: "upKelp2",
+        x: 540,
+        y: 0
+        //img: new Image()
+    }
+];
 
-kelp[0] = {
-    name: "downKelp",
-    x: 300,
-    y: 200
-    //img: new Image()
-}
-
-kelp[1] = {
-    name: "upKelp",
-    x: 300,
-    y: 0
-    //img: new Image()
-}
-
+//캔버스 초기화
 window.onload = function(){
     canvas = document.getElementById('game');
     if(canvas.getContext){
@@ -38,6 +53,7 @@ window.onload = function(){
     }
 }
 
+//오브젝트 생성
 function drawGameObject(gameObj){
     ctx.beginPath();
     if(gameObj.img != null){
@@ -48,6 +64,7 @@ function drawGameObject(gameObj){
     ctx.stroke();
 }
 
+//고래 생성
 function drawWhale(){
     if(whale.status==1){
         whale.status = 2;
@@ -56,62 +73,90 @@ function drawWhale(){
     }
     whale.imgSrc = "asset/img/whale"+whale.status+".png"
     whale.img.src = whale.imgSrc;
-    clearCanvas();
     drawGameObject(whale);
 }
 
+//장애물 생성
 function drawKelp(){
-    drawGameObject(kelp[1]);
     drawGameObject(kelp[0]);
+    drawGameObject(kelp[1]);
+    drawGameObject(kelp[2]);
+    drawGameObject(kelp[3]);
 }
 
+// 캔버스 클리어
 function clearCanvas(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function moveWhale(direction){
+// 고래 위치 계산
+function calcWhale(direction){
     switch(direction){
-        case 'up': whale.y-=35; break;
+        case 'up': whale.y-=30; break;
         case 'down': whale.y+=7; break;
-        // case 'right': whale.x+=10; break;
-        // case 'left': whale.x-=10; break;
     }
-    drawWhale();
 }
 
-function moveKelp(){
+// 장애물 위치 계산
+function calcKelp(){
     if(kelp[0].x < -50 || kelp[1].x < -50){
         kelp[0].x = 300;
         kelp[1].x = 300;
     }
-    kelp[0].x -= 10;
-    kelp[1].x -= 10;
+    if(kelp[2].x < -50 || kelp[3].x < -50){
+        kelp[2].x = 300;
+        kelp[3].x = 300;
+    }
+    kelp[0].x -= 20;
+    kelp[1].x -= 20;
+    kelp[2].x -= 20;
+    kelp[3].x -= 20;
+}
+
+// 오브젝트 위치 계산
+function calcObjPos() {
+    calcWhale('down');
+    if(upBtnClicked) {
+        calcWhale('up');
+    }
+    calcKelp();
+}
+
+//렌더링
+function Render(){
+    clearCanvas();
+    drawWhale();
     drawKelp();
 }
 
-function Render(){
-    clearCanvas();
-    moveWhale('down');
-    moveKelp();
+function upBtnEvent() {
+    upBtnClicked = true;
 }
 
 function startGame(){
     gameInit();
     var game = setInterval(function(){
+        calcObjPos();
         Render();
-        if(whale.y >= 260 || whale.y <= 0 || whale.isCollision(kelp[0]) || whale.isCollision(kelp[1])){
+
+        // 충돌처리
+        if(whale.y > 300 || whale.y < -40 || whale.isCollision(kelp[0]) || whale.isCollision(kelp[1]) || whale.isCollision(kelp[2]) || whale.isCollision(kelp[3])){
             alert("게임오버");
             clearInterval(game);
-            clearCanvas();
         }
+        upBtnClicked = false;
     }, 200);
 }
 
+// 게임 초기화
 function gameInit(){
+    score = 0;
     whale.x = 50;
     whale.y = 50;
     kelp[0].x = 300;
     kelp[1].x = 300;
+    kelp[2].x = 500;
+    kelp[3].x = 500;
     whale.imgSrc = "asset/img/whale1.png";
     whale.img.src = whale.imgSrc;
 }
